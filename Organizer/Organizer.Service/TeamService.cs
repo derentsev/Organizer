85994e84c4832b92b183d88e36131faafb9a2213
Dimitar Derentsev <d.derentsev@abv.bg>
2016-12-22 11:39:39 +0200

    Missing DLL - fixed

---
+++ Organizer/Organizer.Service/TeamService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Organizer.DAL;
+using Organizer.Service;
+
+namespace Service
+{
+    public class TeamService
+        
+    {
+        OrganizerDBEntitiesUpdated TeamContext = new OrganizerDBEntitiesUpdated();
+
+        public string GetTeamByID(int teamID)
+        {
+            //If not working use DTO
+            return  Convert.ToString(TeamContext.team_table.Find(teamID));
+           
+        }
+
+        public void AddNewTeam(string newTeamName)
+        {
+            TeamContext.spCreateTeam(newTeamName);
+        }
+
+        public void DeleteTeam(int teamID)
+        {
+            TeamContext.spDeleteTeamByID(teamID);
+        }
+
+
+        public void UpdateTeam(string updateTeamName, int teamID)
+        {
+            TeamContext.spUpdateTeamByID(updateTeamName, teamID);
+        }
+    }
+}
---
+++ Organizer/Organizer.Service/TeamTaskService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Organizer.DAL;
+using Organizer.Service;
+
+namespace Service
+{
+    public class TeamTaskService
+    {
+        OrganizerDBEntitiesUpdated TeamTaskContext = new OrganizerDBEntitiesUpdated();
+
+        public void InsertTeamTask(int teamID, int taskID)
+        {
+            TeamTaskContext.spInsertTeamTask(teamID, taskID);
+        }
+
+        public void DeleteTeamTask(int taskID, int teamID)
+        {
+            TeamTaskContext.spDeleteTeamTask(teamID, taskID);
+        }
+    }
+}
